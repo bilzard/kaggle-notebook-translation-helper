1e02d18bdf9ba2e677de0eef8077d4b5b8dec9e1
bilzard <nakamine11@gmail.com>
2022-11-06 04:38:35 +0900

    bug fix: adapt to SPA

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,43 +1,7 @@
-// adopted from: https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
-function waitForElement(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-}
-
 const pageUrl = window.location.href;
-const kaggle_notebooks = "https://www.kaggle.com/code/";
-const notebooks = "https://www.kaggleusercontent.com/";
+const notebook_source_url = "https://www.kaggleusercontent.com/";
 
-if (pageUrl.startsWith(kaggle_notebooks)) {
-    waitForElement("iframe#rendered-kernel-content").then((iframe) => {
-        const sourceCodeUrl = iframe.src;
-        const linkButton = document.createElement("button");
-        linkButton.id = "link-to-source";
-        linkButton.innerHTML = "Open souce code in a new tab";
-        linkButton.onclick = () => {
-            window.open(sourceCodeUrl, '_blank').focus();
-        };
-        waitForElement("div[data-testid='notebook-detail-render-tid']").then((notebook) => {
-            notebook.prepend(linkButton);
-        });
-    })
-}
-if (pageUrl.startsWith(notebooks)) {
+if (pageUrl.startsWith(notebook_source_url)) {
     // enable scroll
     document.querySelector("body").style.overflowY = "auto";
 
