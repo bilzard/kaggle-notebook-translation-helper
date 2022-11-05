0777b7f3ef54c207000163c69b0fd7a1803215a5
bilzard <nakamine11@gmail.com>
2022-11-06 04:38:35 +0900

    bug fix: adapt to SPA

---
+++ src/background.js
@@ -0,0 +1,14 @@
+const kaggle_notebook_url = "https://www.kaggle.com/code/";
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === "complete" && tab.url.startsWith(kaggle_notebook_url)) {
+        chrome.scripting.insertCSS({
+            files: ["styles/content.css"],
+            target: { tabId: tab.id },
+        });
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["scripts/kaggle_notebook.js"],
+        });
+    }
+});
