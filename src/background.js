da0d1c5d95ceb2774150dc2822b1f472b9b860a5
bilzard <nakamine11@gmail.com>
2022-11-07 14:20:29 +0900

    disable on code list url: */code/

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 const kaggle_notebook_url = "https://www.kaggle.com/code/";
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete" && tab.url.startsWith(kaggle_notebook_url) && (!tab.url.endsWith("/edit"))) {
+    if (changeInfo.status === "complete" && tab.url.startsWith(kaggle_notebook_url) && (!tab.url.endsWith("/edit")) && (tab.url != kaggle_notebook_url)) {
         chrome.scripting.insertCSS({
             files: ["styles/content.css"],
             target: { tabId: tab.id },
