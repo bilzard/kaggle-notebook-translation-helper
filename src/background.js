f7dcb6e48548ca64c83602b3ca01d5c1a161c5b5
bilzard <nakamine11@gmail.com>
2022-11-07 16:39:34 +0900

    refactor

--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@ const kaggle_notebook_url = "https://www.kaggle.com/code/";
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === "complete" && tab.url.startsWith(kaggle_notebook_url) && (!tab.url.endsWith("/edit")) && (tab.url != kaggle_notebook_url)) {
         chrome.scripting.insertCSS({
-            files: ["styles/content.css"],
+            files: ["styles/kaggle_notebook.css"],
             target: { tabId: tab.id },
         });
         chrome.scripting.executeScript({
