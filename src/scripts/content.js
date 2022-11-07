6b238bdfe4bc946a90e7e6f1c992221de7f81259
bilzard <nakamine11@gmail.com>
2022-11-07 16:55:43 +0900

    refactor: define style in css file

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,9 +2,6 @@ const pageUrl = window.location.href;
 const notebook_source_url = "https://www.kaggleusercontent.com/";
 
 if (pageUrl.startsWith(notebook_source_url)) {
-    // enable scroll
-    document.querySelector("body").style.overflowY = "auto";
-
     // disable translation on code
     const code_cells = document.querySelectorAll("div.code_cell");
     for (const code_cell of code_cells) {
