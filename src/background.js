805fa3199d1b72ad22be56d954c6c70e57fad348
bilzard <nakamine11@gmail.com>
2023-01-12 15:38:36 +0900

    Experimental: disable translation for MathJax equations

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 const kaggle_notebook_url = "https://www.kaggle.com/code/";
+const usercontent_url = "https://www.kaggleusercontent.com/";
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === "complete" && tab.url.startsWith(kaggle_notebook_url) && (!tab.url.endsWith("/edit")) && (tab.url != kaggle_notebook_url)) {
@@ -9,6 +10,16 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         chrome.scripting.executeScript({
             target: { tabId: tabId },
             files: ["scripts/kaggle_notebook.js"],
+        });
+    }
+    else if (changeInfo.status === "complete" && tab.url.startsWith(usercontent_url)) {
+        chrome.scripting.insertCSS({
+            files: ["styles/content.css"],
+            target: { tabId: tab.id },
+        });
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["scripts/content.js"],
         });
     }
 });
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,6 +1,45 @@
+function waitForElements(selector, rootNode = document.body) {
+    return new Promise(resolve => {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (typeof node.matches === "function" && node.matches(selector)) {
+                        // TODO: suppress unnecessary function calls
+                        resolve(document.querySelectorAll(selector));
+                        observer.disconnect();
+                    }
+                });
+            });
+        });
+
+        observer.observe(rootNode, {
+            attributes: false,
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 // disable translation on code & UI elements
-document.querySelectorAll("div.code_cell").forEach(code_cell => {
-    code_cell.classList.add("notranslate");
+document.querySelectorAll("pre").forEach(node => {
+    console.log("Code element was found")
+    node.classList.add("notranslate");
+});
+
+/**
+ * Experimental: set `notranslate` class for MathJax Elements
+ *
+ * current issues:
+ *   - redundant function call
+ *   - optimal delay time varies in client environments
+ */
+waitForElements("span.MathJax").then(nodes => {
+    setTimeout(() => {
+        nodes.forEach(node => {
+            console.log("MathJax element was found")
+            node.classList.add("notranslate");
+        });
+    }, 500);
 });
 
 // remove button elements
